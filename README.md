f920c25e73cdcd7b1b1c0b1e983a3a701c2b0dd4
Allan Lima <allan.moura@gmail.com>
2024-12-09 14:50:10 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Estudando com Felipe Deschamps
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Vamos juntos impactar o mundo....vem comigo??? :-) </h1>
+  return <h1>Vamos juntos impactar o mundo....vem comigo??? :-) </h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
 }
 
 export default Home;
