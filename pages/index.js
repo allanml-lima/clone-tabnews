4abe629664b5fb14e7fdc4fb8810417ae5c39c2f
Allan Lima <allan.moura@gmail.com>
2024-12-13 23:55:03 -0300

    add `compose` config file with database service

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Vamos juntos impactar o mundo....vem comigo??? :-) </h1>;
 }
 
-function teste() {
-  console.log("teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
